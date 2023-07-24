cd6331a866ab1da3042a1795a89f224a46c29b44
techchrism <chris@techchrism.me>
2023-07-23 19:48:30 -0700

    add single-file build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import {viteSingleFile} from 'vite-plugin-singlefile'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
         */
         // devtools(),
         solidPlugin(),
+        viteSingleFile(),
     ],
     server: {
         port: 3000,
